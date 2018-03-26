d9a84a807e67328211da8f5e00e5cbf1ced229dd
Vanessa Toro <vanetoro84@gmail.com>
2018-03-26 00:13:23 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -37,6 +37,7 @@ def alan_kay_is_known_for
           :languages => ["C"]
         }
      }
+     programmer_hash[:alan_kay][:known_for]
 end
 
 def dennis_ritchies_language
@@ -55,7 +56,7 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
-     programmer_hash[:alan_kay][:known_for]
+     
 end
 
 def adding_matz
