a7b71f6f5946847ec8e17a7ebeda39351fa1001c
Vanessa Toro <vanetoro84@gmail.com>
2018-03-26 00:45:13 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -102,9 +102,9 @@ def changing_alan
         }
      }
      
-
+    programmer_hash[:alan_kay][:known_for] = "GUI"
      #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
-     alans_new_info = "GUI"
+     
      
      
 end
